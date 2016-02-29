4a6c85eaa9e67a8c7cbcb28ef4e7877e3b9ef669
Mahesh Vangala <vangalamaheshh@gmail.com>
2016-02-29 15:13:07 -0500

    added skeletion partials

--- app/js/app.js
+++ app/js/app.js
@@ -19,6 +19,50 @@ mbcfWebApp.config(['$stateProvider', '$urlRouterProvider', '$locationProvider',
             url: '/instrumentation',
             templateUrl: '/app/partials/instrumentation.html'
         })
+        .state('/ngs', {
+            url: '/ngs',
+            templateUrl: '/app/partials/ngs.html'
+        })
+        .state('/ngs-lib-prep', {
+            url: '/ngs-lib-prep',
+            templateUrl: '/app/partials/ngs-lib-prep.html'
+        })
+        .state('/microarray', {
+            url: '/microarray',
+            templateUrl: '/app/partials/microarray.html'
+        })
+        .state('/nano', {
+            url: '/nano',
+            templateUrl: '/app/partials/nano.html'
+        })
+        .state('/sanger', {
+            url: '/sanger',
+            templateUrl: '/app/partials/sanger.html'
+        })
+        .state('/maldi', {
+            url: '/maldi',
+            templateUrl: '/app/partials/maldi.html'
+        })
+        .state('/ms', {
+            url: '/ms',
+            templateUrl: '/app/partials/ms.html'
+        })
+        .state('/amino', {
+            url: '/amino',
+            templateUrl: '/app/partials/amino.html'
+        })
+        .state('/peptide-synthesis', {
+            url: '/peptide-synthesis',
+            templateUrl: '/app/partials/peptide-synthesis.html'
+        })
+        .state('/biacore', {
+            url: '/biacore',
+            templateUrl: '/app/partials/biacore.html'
+        })
+        .state('/adv-tech', {
+            url: '/adv-tech',
+            templateUrl: '/app/partials/adv-tech.html'
+        })
         .state('/', {
             url: '/',
             templateUrl: '/app/partials/instrumentation.html'
