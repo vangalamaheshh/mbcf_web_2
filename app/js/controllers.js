d320703a61bdfc7f2092a7af51b201b3d321be61
Mahesh Vangala <vangalamaheshh@gmail.com>
2016-03-07 16:12:06 -0500

    added if else condition to make sure the form fields are filled

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -7,7 +7,17 @@ mbcfWebAppControllers.controller( 'EmailController', ['$scope', '$http', functio
 	$scope.user_email = "";
 	$scope.message = "";
 	$scope.sendEmail = function( ) {
-		console.log( "Name: " + $scope.user_name + ";" + "Email: " + $scope.user_email + ": Message: " + $scope.message )
+        if( ! $scope.user_name ){
+            alert("Name field is required");
+        }
+        else if( ! $scope.user_email ) {
+            alert("Email field is required");
+        }
+        else if( ! $scope.message ) {
+            alert("Please enter a message");
+        }
+        else {
+		//console.log( "Name: " + $scope.user_name + ";" + "Email: " + $scope.user_email + ": Message: " + $scope.message )
 		var data = ({
 			contact_name : $scope.user_name,
 			contact_email : $scope.user_email,
@@ -16,9 +26,13 @@ mbcfWebAppControllers.controller( 'EmailController', ['$scope', '$http', functio
 		$http.post('/', data).
 			success(function(data, status, header, config) {
 				alert("Thanks for submitting your question. We will respond to you as soon as possible.");
+                $scope.user_name = "";
+                $scope.user_email = "";
+                $scope.message = "";
 			}).
            	error(function(data, status, headers, config) {
             	alert("The email is not sent. Error code: " + status);
             });
+        }
 	};
 }]);
